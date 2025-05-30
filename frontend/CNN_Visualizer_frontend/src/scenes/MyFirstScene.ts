8260d9483f9ea0500a100c4b8e2551ad9f60eaf6
Elmrys <elmrys@gmail.com>
2025-05-30 16:41:29 +0200

    testing text size and dpi rendering

--- frontend/CNN_Visualizer_frontend/src/scenes/MyFirstScene.ts
+++ frontend/CNN_Visualizer_frontend/src/scenes/MyFirstScene.ts
@@ -95,9 +95,9 @@ const ANIMATION_TEXT_PLACEMENTS = [
     {vertical_alignment: Control.VERTICAL_ALIGNMENT_TOP, horizontal_alignment: Control.HORIZONTAL_ALIGNMENT_LEFT, left: "0%", top: "0%", width: "25%", height: "auto"},
     {vertical_alignment: Control.VERTICAL_ALIGNMENT_TOP, horizontal_alignment: Control.HORIZONTAL_ALIGNMENT_RIGHT, left: "0%", top: "0%", width: "40%", height: "auto"},
     {vertical_alignment: Control.VERTICAL_ALIGNMENT_TOP, horizontal_alignment: Control.HORIZONTAL_ALIGNMENT_RIGHT, left: "0%", top: "0%", width: "45%", height: "auto"},
-    {vertical_alignment: Control.VERTICAL_ALIGNMENT_TOP, horizontal_alignment: Control.HORIZONTAL_ALIGNMENT_CENTER, left: "0%", top: "10%", width: "50%", height: "auto"},
-    {vertical_alignment: Control.VERTICAL_ALIGNMENT_TOP, horizontal_alignment: Control.HORIZONTAL_ALIGNMENT_RIGHT, left: "0%", top: "10%", width: "60%", height: "auto"},
-    {vertical_alignment: Control.VERTICAL_ALIGNMENT_CENTER, horizontal_alignment: Control.HORIZONTAL_ALIGNMENT_LEFT, left: "5%", top: "0%", width: "25%", height: "auto"},
+    {vertical_alignment: Control.VERTICAL_ALIGNMENT_TOP, horizontal_alignment: Control.HORIZONTAL_ALIGNMENT_CENTER, left: "0%", top: "0%", width: "50%", height: "auto"},
+    {vertical_alignment: Control.VERTICAL_ALIGNMENT_TOP, horizontal_alignment: Control.HORIZONTAL_ALIGNMENT_RIGHT, left: "0%", top: "0%", width: "60%", height: "auto"},
+    {vertical_alignment: Control.VERTICAL_ALIGNMENT_CENTER, horizontal_alignment: Control.HORIZONTAL_ALIGNMENT_LEFT, left: "0%", top: "0%", width: "25%", height: "auto"},
     {vertical_alignment: Control.VERTICAL_ALIGNMENT_BOTTOM, horizontal_alignment: Control.HORIZONTAL_ALIGNMENT_CENTER, left: "0%", top: "0%", width: "95%", height: "auto"},
 ]
 
